513e6c66f3fe19dba831622934483b4a765f8ffc
Lucca <luccazvps@gmail.com>
2023-01-20 14:30:50 -0300

    feat: add exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { allFilters } from './commom/filters/http-exception.filter';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalFilters(new allFilters());
   await app.listen(3000);
 
   if (module.hot) {
