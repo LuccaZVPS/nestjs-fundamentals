b8ec327f23bd252d930e24ec5f99646fcdfc670f
Lucca <luccazvps@gmail.com>
2023-01-21 12:17:54 -0300

    feat: add new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PlayersModule } from './players/players.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from './categories/categories.module';
+import { ChallengesModule } from './challenges/challenges.module';
 @Module({
   imports: [
     PlayersModule,
     MongooseModule.forRoot(
       'mongodb+srv://admin:7THzq8jsjCf6a6pT@cluster0.46uycfg.mongodb.net/?retryWrites=true&w=majority',
     ),
+    CategoriesModule,
+    ChallengesModule,
   ],
   controllers: [],
   providers: [],
